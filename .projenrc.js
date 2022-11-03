fc7ad2d4f2aa1560d5bb2db4f87e1fa888a9c96b
Matt Wise <matt@nextdoor.com>
2022-11-03 06:09:26 -0700

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,16 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Matt Wise',
+  authorAddress: 'wise@wiredgeek.net',
+  authorName: 'Matt Wise',
+  cdkVersion: '2.50.0',
+  defaultReleaseBranch: 'main',
+  name: 'coremodule',
+  repositoryUrl: 'https://github.com/diranged/JSII3000-coremodule-example.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
